20980886a588d9bf31004f15c1a763763eaf4fc5
Bradner <bradnerc@hotmail.ca>
2023-03-16 15:26:23 -0700

    adding tensor as class library

--- app/src/main/java/com/example/foodfacts/MainActivity.kt
+++ app/src/main/java/com/example/foodfacts/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.foodfacts
 
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val cn = ComponentName(this, "org.tensorflow.lite.examples.imageclassification.MainActivity")
+//        val intent = Intent().setComponent(cn)
+//        startActivity(intent)
+
+        val intent = Intent(this, org.tensorflow.lite.examples.imageclassification.MainActivity::class.java)
+        //startActivity(intent)
     }
 }
