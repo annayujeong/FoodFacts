4f29d87d59911b327a591b525506d5be2c285d7b
Bradner <bradnerc@hotmail.ca>
2023-03-20 14:39:57 -0700

    minor changes

--- app/src/main/java/com/example/foodfacts/FavouriteFoodsFragment.kt
+++ app/src/main/java/com/example/foodfacts/FavouriteFoodsFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [splashScreenFragment.newInstance] factory method to
+ * Use the [FavouriteFoodsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class splashScreenFragment : Fragment() {
+class FavouriteFoodsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,27 +34,7 @@ class splashScreenFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val loginButton = view.findViewById<Button>(R.id.loginButton)
-
-        val createAccountButton = view.findViewById<Button>(R.id.createAccountButton)
-
-        loginButton.setOnClickListener(){
-            // TODO: call method to sign in
-            val email = view.findViewById<EditText>(R.id.editTextTextEmailAddress)
-            val password = view.findViewById<EditText>(R.id.editTextTextPassword)
-
-
-        }
-
-        createAccountButton.setOnClickListener(){
-            // TODO: call method to create account
-        }
+        return inflater.inflate(R.layout.fragment_favourite_foods, container, false)
     }
 
     companion object {
@@ -66,12 +44,12 @@ class splashScreenFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment splashScreenFragment.
+         * @return A new instance of fragment FavouriteFoodsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            splashScreenFragment().apply {
+            FavouriteFoodsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/foodfacts/FoodViewModel.kt
+++ app/src/main/java/com/example/foodfacts/FoodViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.foodfacts
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 // ViewModel responsible for handling calls/other stuff to the Firebase database
 // handling user information and account?
@@ -9,11 +12,26 @@ class FoodViewModel: ViewModel() {
 
     // TODO: sign in and account creation here?
 
-    fun createAccount(){
+    lateinit var db: FirebaseFirestore
 
+    fun initializeFirebase(){
+        db = Firebase.firestore
+    }
+
+    fun createAccount(){
+        // TODO:
     }
 
     fun signIn(){
-
+        // TODO:
     }
+
+    fun getFavouriteItems(){
+        // TODO:
+    }
+
+    fun addToFavouriteItems(){
+        // TODO:
+    }
+
 }
