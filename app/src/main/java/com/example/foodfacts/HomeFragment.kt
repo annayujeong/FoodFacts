de5db3ff5bfda5c7c260e1b4b9515ae18108b47d
Bradner <bradnerc@hotmail.ca>
2023-04-01 01:37:08 -0700

    adding logo, cleaning up ui

--- app/src/main/java/com/example/foodfacts/HomeFragment.kt
+++ app/src/main/java/com/example/foodfacts/HomeFragment.kt
@@ -1,20 +1,14 @@
 package com.example.foodfacts
 
 import android.os.Bundle
-import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.View.OnAttachStateChangeListener
 import android.widget.Button
-import android.widget.EditText
 import android.widget.SearchView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.onNavDestinationSelected
 import com.example.foodfacts.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -39,16 +33,21 @@ class HomeFragment : Fragment() {
 
         val apiViewModel: ApiViewModel by activityViewModels()
 
-        val button = view.findViewById<Button>(R.id.button_home)
-        val foodButton = view.findViewById<Button>(R.id.button_listItem_GoToList)
+        //val button = view.findViewById<Button>(R.id.button_homeFragment_search)
+        val button = binding.buttonHomeFragmentSearch
 
-        val text = view.findViewById<TextView>(R.id.textView_food_name_home).text.toString()
+        //val foodButton = view.findViewById<Button>(R.id.button_homeFragment_favourites)
+        val foodButton = binding.buttonHomeFragmentFavourites
+
+        //val text = view.findViewById<TextView>(R.id.textView_food_name_home).text.toString()
 
         foodButton.setOnClickListener{
             findNavController().navigate(R.id.action_homeFragment_to_foodItemListFragment)
         }
 
-        val search = view.findViewById<SearchView>(R.id.searchView)
+        //val search = view.findViewById<SearchView>(R.id.searchView_homeFragment)
+        val search = binding.searchViewHomeFragment
+
         search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 apiViewModel.getDataAndNavigateToResult(search.query.toString(), findNavController())
@@ -60,7 +59,7 @@ class HomeFragment : Fragment() {
         })
 
         button.setOnClickListener {
-            apiViewModel.getDataAndNavigateToResult(text, findNavController())
+            apiViewModel.getDataAndNavigateToResult(search.query.toString(), findNavController())
         }
     }
 
